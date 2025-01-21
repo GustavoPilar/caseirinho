b9288b17f71a51d0f4c32b8a02e36dcd351648a9
GustavoPilar <pilargustavo7@gmail.com>
2025-01-20 23:20:51 -0300

    Adição de navbar Hamburger

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function redirect(dataPage) {
     window.location.href = `pages/receitas/receita.html?page=${dataPage}`;
 }
+
+function showOptions() {
+    const navbar = document.getElementById('navOption');
+    navbar.classList.toggle('d-none');
+    navbar.classList.toggle('d-block');
+}
